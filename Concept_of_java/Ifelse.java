d362b90664f611c4b9a9602ec3e077e471b132d0
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-09-03 20:05:35 +0000

    Check whether the number is even or not.

--- Concept_of_java/Ifelse.java
+++ Concept_of_java/Ifelse.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Ifelse {
-    public static void(String[] args)
+    public static void main(String[] args)
     {
-        Scanner.sc = 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = sc.nextInt();
+        if(number%2==0)
+        {
+            System.out.print(number+" is even");
+        }
+        else{
+            System.out.print(number+" is odd");
+        }
+        sc.close();
     }
     
 }
