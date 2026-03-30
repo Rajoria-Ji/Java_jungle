cb3276ac40b5c55a36a5d6d9047517f1fbeffdbe
Sumit Rajoria <sumitrajoria@Mac.lan>
2026-03-31 00:04:12 +0530

    stack

---
+++ Stacking/MyStack.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class MyStack {
+    public static void main(String[] args) {
+        
+
+        
+        Stack<Integer> st = new Stack<>();
+
+        st.push(5);
+        st.push(6);
+        st.push(7);
+        st.push(20);
+
+     
+        while(!st.isEmpty()){
+            if(st.peek() == 7) {
+            System.out.println("True");
+            break;
+        }        
+        st.pop();
+    }
+}
+}
